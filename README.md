a10edac14a0edf46432b3579231c067413b97348
chideraike <chideraike.s@gmail.com>
2020-04-11 18:13:55 +0100

    Initial commit of the game

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guess A Number
+This game takes your number **(from 1-99)**, and it tries to guess it.
+It shows the _number of rounds_ taken to guess your number.
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import MainButton from '../components/MainButton';
+import Colors from '../constants/colors';
+
+const GameOverScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <TitleText>The Game is Over!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/success.png')}
+                    //source={{ uri: 'https://get.pxhere.com/photo/mountain-snow-winter-adventure-mountain-range-weather-alpine-extreme-sport-season-ridge-summit-mountain-summit-mountaineering-winter-sport-sports-mountaineer-alps-cirque-piste-landform-ski-touring-allg-u-alps-ski-mountaineering-great-wilder-mountainous-landforms-geological-phenomenon-ar-te-allgau-oytal-1038627.jpg' }}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>
+                    Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text>.
+                </BodyText>
+            </View>
+            <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold'
+    }
+});
+
+export default GameOverScreen;
